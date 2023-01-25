39ca289ece08f188b79305d3f35cce741b1e7e36
Jörg Thalheim <joerg@thalheim.io>
2023-01-25 09:34:59 +0100

    fixup typing

--- default.nix
+++ default.nix
@@ -15,21 +15,7 @@ buildPythonApplication rec {
   name = "bme680-mqtt";
   inherit src;
   propagatedBuildInputs = [
-    (bme680.override {
-      smbus-cffi = smbus-cffi.overrideAttrs (old: {
-        # bug in fetchpatch?
-        preBuild = ''
-          touch smbus/py.typed
-        '';
-        patches = old.patches ++ [
-          # https://github.com/bivab/smbus-cffi/pull/25
-          (fetchpatch {
-            url = "https://github.com/bivab/smbus-cffi/commit/9e72d80e4d8362c966dc65270f43a6ab9e703578.patch";
-            sha256 = "sha256-Tsq+6PIw/3o/GXo73Nza7USws6UAeG2u4slRB0KQR6U=";
-          })
-        ];
-      });
-    })
+    bme680
     paho-mqtt
   ];
   nativeCheckInputs = [
@@ -42,7 +28,7 @@ buildPythonApplication rec {
     echo -e "\x1b[32m## run black\x1b[0m"
     black --check .
     echo -e "\x1b[32m## run ruff\x1b[0m"
-    ruff --check bme680_mqtt
+    ruff .
     echo -e "\x1b[32m## run mypy\x1b[0m"
     mypy bme680_mqtt
   '';
