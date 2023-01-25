3b4972bca80c1f3f6378faedafa0a951faab2b01
Jörg Thalheim <joerg@thalheim.io>
2023-01-25 09:14:39 +0100

    fix checks and switch to ruff

--- default.nix
+++ default.nix
@@ -1,14 +1,14 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz") {}
-, python3 ? pkgs.python3
-, mypy ? pkgs.mypy
-, glibcLocales ? pkgs.glibcLocales
-, buildPythonApplication ? python3.pkgs.buildPythonApplication
-, black ? python3.pkgs.black
-, flake8 ? python3.pkgs.flake8
-, bme680 ? python3.pkgs.bme680
-, smbus-cffi ? python3.pkgs.smbus-cffi
-, paho-mqtt ? python3.pkgs.paho-mqtt
-, src ? ./.
+{ fetchpatch
+, python3
+, mypy
+, glibcLocales
+, buildPythonApplication
+, black
+, ruff
+, bme680
+, smbus-cffi
+, paho-mqtt
+, src
 }:
 
 buildPythonApplication rec {
@@ -23,7 +23,7 @@ buildPythonApplication rec {
         '';
         patches = old.patches ++ [
           # https://github.com/bivab/smbus-cffi/pull/25
-          (pkgs.fetchpatch {
+          (fetchpatch {
             url = "https://github.com/bivab/smbus-cffi/commit/9e72d80e4d8362c966dc65270f43a6ab9e703578.patch";
             sha256 = "sha256-Tsq+6PIw/3o/GXo73Nza7USws6UAeG2u4slRB0KQR6U=";
           })
@@ -32,17 +32,17 @@ buildPythonApplication rec {
     })
     paho-mqtt
   ];
-  checkInputs = [ 
+  nativeCheckInputs = [
     mypy
-    glibcLocales
     black
-    flake8
+    ruff
   ];
+
   checkPhase = ''
     echo -e "\x1b[32m## run black\x1b[0m"
-    LC_ALL=en_US.utf-8 black --check .
-    echo -e "\x1b[32m## run flake8\x1b[0m"
-    flake8 bme680_mqtt
+    black --check .
+    echo -e "\x1b[32m## run ruff\x1b[0m"
+    ruff --check bme680_mqtt
     echo -e "\x1b[32m## run mypy\x1b[0m"
     mypy bme680_mqtt
   '';
